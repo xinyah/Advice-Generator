d2046d73810138ca6e819049e3e0050fe78cd0d4
xinyah <xinyah.work@gmail.com>
2022-04-08 01:48:09 -0400

    Simple Advice Generator

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import "./App.css";
+import lowerBanner from "./pattern-divider-desktop.svg";
+import buttonImg from "./icon-dice.svg";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const [adviceNum, setAdviceNum] = useState(-1);
+	const [advice, setAdvice] = useState("");
+
+	useEffect(() => {
+		getNewAdvice();
+	}, []);
+
+	const getNewAdvice = () => {
+		fetch("https://api.adviceslip.com/advice")
+			.then((res) => res.json())
+			.then((result) => {
+				setAdviceNum(result.slip.id);
+				setAdvice(result.slip.advice);
+				console.log(result.slip);
+			});
+	};
+
+	return (
+		<div class="advice-box">
+			<p>Advice #{adviceNum}</p>
+			<p>"{advice}"</p>
+			<img class="first-image" src={lowerBanner} alt="deco" />
+			<div class="second-image" onClick={getNewAdvice}>
+				<img src={buttonImg} alt="dice" />
+			</div>
+		</div>
+	);
 }
 
 export default App;
